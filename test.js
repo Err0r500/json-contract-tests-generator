68181c0cee5a33a930b8329d1f99c7bfa9842ea7
Matthieu Jacquot <matthieu.jacquot@schibsted.com>
2018-02-21 19:25:54 +0100

    dataset construction first draft

--- test.js
+++ test.js
@@ -2,31 +2,99 @@ var fs = require('fs');
 
 var contents = fs.readFileSync('testdata/simple.json');
 
-const buildCustomType = (obj) => {
-    // console.log("generate >> ", obj["type"], obj)
+class generator {
+    constructor(funcToApply, value) {
+        this.funcToApply = funcToApply;
+        this.value = value;
+    }
+
+    get generate() {
+        return this.funcToApply(this.value);
+    }
 }
+
+
+class intWithMinConstrain extends generator {
+    constructor(value) {
+        super()
+        this.value = value;
+    }
+
+    valid(min) {
+        return min + 1
+    }
+
+    invalid(min) {
+        return min - 1
+    }
+
+    get generateValid() {
+        return new generator(this.valid, this.value).generate
+    }
+    get generateInValid() {
+        return new generator(this.invalid, this.value).generate
+    }
+}
+
+class intWithMaxConstrain extends generator {
+    constructor(value) {
+        super()
+        this.value = value;
+    }
+
+    valid(min) {
+        return min - 1
+    }
+
+    invalid(min) {
+        return min + 1
+    }
+
+    get generateValid() {
+        return new generator(this.valid, this.value).generate
+    }
+    get generateInValid() {
+        return new generator(this.invalid, this.value).generate
+    }
+}
+
 
 const traverseObject = (obj) => {
     var tmp = {}
     Object.keys(obj).forEach(key => {
         if (key == "properties") {
             tmp = traverseObject(obj[key]) // recurse on root object
+        } else
+        if (key == "required") {
+            tmp["required_fields"] = obj[key] // add required fields to root object
         } else if (obj[key]["properties"]) {
             tmp[key] = traverseObject(obj[key]["properties"]) // recurse on next level object
         } else if (typeof obj[key] == 'object') {
             tmp[key] = obj[key]["type"];
+            if (obj[key]["type"] == "integer") {
+                tmp[key] = [new intWithMinConstrain(12), new intWithMaxConstrain(1001)]
+            }
         }
     });
     return tmp
 };
 
 var final = traverseObject(JSON.parse(contents))
+var dataset = []
 
-console.log(final)
-// fs.writeFile("/tmp/test", JSON.stringify(final), function (err) {
-//     if (err) {
-//         return console.log(err);
-//     }
+// todo recursive contruction too
+const buildSet = (obj, valid) => {
+    Object.keys(obj).forEach(key => {
+        if (typeof obj[key].isArray) {
+            for (let i = 0; i < obj[key].length; i++) {
+                var tmpObj = JSON.parse(JSON.stringify(final))
+                if (valid) tmpObj[key] = obj[key][i].generateValid;
+                else tmpObj[key] = obj[key][i].generateValid;
+                dataset.push(tmpObj)
+            }
+        }
+    });
+};
 
-//     console.log("The file was saved!");
-// });
+buildSet(final, true)
+console.log(dataset)
